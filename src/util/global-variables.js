8a4520a8aa3e59a4f76b718a7eb9276844bd2ebc
Gino <romangino@gmail.com>
2022-11-29 00:31:13 -0500

    update global-variables.js domain_address variable to use environment variable instead

--- src/util/global-variables.js
+++ src/util/global-variables.js
@@ -1,4 +1,4 @@
-const Domain_address = "http://localhost:4000";
+const Domain_address = process.env.REACT_APP_BASE_URL;
 
 export const Search_API = `${Domain_address}/search/coinSearch?query=`;
 export const CoinData_API = `${Domain_address}/detail/getCoinData?coinID=`;
