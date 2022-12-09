7ee2998d66a726f271a833528772d08ef17161d5
jaxtan <niftyjax@gmail.com>
2022-12-09 16:22:15 -0500

    likes and dislikes are fully loaded

--- src/detail-screen/comment-item.js
+++ src/detail-screen/comment-item.js
@@ -1,15 +1,22 @@
 import React from "react";
 import {useDispatch, useSelector} from "react-redux";
-import {deleteGivenCommentThunk} from "../services/comment-thunk";
+import {
+    createNewUCRecordThunk,
+    deleteGivenCommentThunk,
+    deleteUCRecordThunk,
+    updateUCRecordThunk
+} from "../services/comment-thunk";
 import Moment from 'react-moment';
-import currentUser from "../users/current-user";
 
 const CommentItem = ({comment}) => {
+
     const {currentUser} = useSelector(state => state.users);
     const {userReactions} = useSelector(state => state.comments)
-    let currentReactionType = 0;  // initially neutral reaction
+    let currentReaction = null;
+    let currentReactionType = 0;  // initially neutral reaction, if this user already react to the comment, it would be 1 or -1
     for (const thisRC of userReactions) {
         if (thisRC.commentID === comment._id) {
+            currentReaction = thisRC;
             currentReactionType = thisRC.reactionType;
             break;
         }
@@ -19,6 +26,50 @@ const CommentItem = ({comment}) => {
     function deleteTuitHandler(commentID) {
         console.log("delete me");
         dispatch(deleteGivenCommentThunk(commentID));
+    }
+
+    function thumbsUpClickHandler() {
+        if (!currentUser) {
+            alert("Only registered member could react to a comment")
+            return;
+        }
+        if (currentReactionType === 1) {
+            // delete reaction
+            dispatch(deleteUCRecordThunk(currentReaction))
+        } else if (currentReactionType === 0) {
+            const newUCRecord = {
+                userID : currentUser._id,
+                commentID : comment._id,
+                reactionType : 1
+            }
+            // create a new reaction
+            dispatch(createNewUCRecordThunk(newUCRecord))
+        } else if (currentReactionType === -1) {
+            // update reaction
+            dispatch(updateUCRecordThunk(currentReaction))
+        }
+    }
+
+    function thumbsDownClickHandler(){
+        if (!currentUser) {
+            alert("Only registered member could react to a comment")
+            return;
+        }
+        if (currentReactionType === -1) {
+            // delete reaction
+            dispatch(deleteUCRecordThunk(currentReaction))
+        } else if (currentReactionType === 0) {
+            const newUCRecord = {
+                userID : currentUser._id,
+                commentID : comment._id,
+                reactionType : -1
+            }
+            // create a new reaction
+            dispatch(createNewUCRecordThunk(newUCRecord))
+        } else if (currentReactionType === 1) {
+            // update reaction
+            dispatch(updateUCRecordThunk(currentReaction))
+        }
     }
 
     return(
@@ -45,12 +96,25 @@ const CommentItem = ({comment}) => {
                 <ul className="list-unstyled list-inline d-flex justify-content-between">
                     <li><i className="fa fa-calendar wd-app-color-yellow"></i>  <Moment fromNow ago>{comment.createdAt}</Moment></li>
                     <li>
-                        <button type={"button"} className={"btn btn-link pt-0 ps-0 text-dark"}>
-                        <i className="fa fa-thumbs-up "></i></button> {comment.likes}
+                        <button type={"button"} className={"btn btn-link pt-0 ps-0 text-dark"} onClick={() => thumbsUpClickHandler()}>
+                            {
+                            currentReactionType === 1
+                                ? <i className="fa fa-thumbs-up wd-app-color-yellow"></i>
+                                : <i className="fa fa-thumbs-up "></i>
+                            }
+                        </button>
                     </li>
+                    <li>{
+                        comment.likes - comment.dislikes
+                    }</li>
                     <li>
-                        <button type={"button"} className={"btn btn-link pt-0 ps-0 text-dark"}>
-                        <i className="fa fa-thumbs-down "></i></button> {comment.dislikes}
+                        <button type={"button"} className={"btn btn-link pt-0 ps-0 text-dark"} onClick={() => thumbsDownClickHandler()}>
+                            {
+                                currentReactionType === -1
+                                    ? <i className="fa fa-thumbs-down text-danger"></i>
+                                    : <i className="fa fa-thumbs-down "></i>
+                            }
+                        </button>
                     </li>
                 </ul>
             </div>
